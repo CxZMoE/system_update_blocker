0dbafe7856be788e6645d90c16a5cd3b6e597ca9
CxZMoE <cxzmoe@aliyun.com>
2023-02-03 17:28:16 +0800

    first commit

---
+++ customize.sh
@@ -0,0 +1,6 @@
+ui_print "### MIUI Update blocker"
+ui_print "Author: CxZMoE"
+ui_print "Stop com.miui.update from connecting to its server."
+
+# 设置hosts文件的权限为0644
+set_perm $MODPATH/system/etc/hosts 0 0 0644
