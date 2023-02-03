566f64c7c66269abcd6e7ca11443dc4f98df9b6b
CxZMoE <cxzmoe@aliyun.com>
2023-02-03 17:33:39 +0800

    fc

---
+++ README.md
@@ -0,0 +1,2 @@
+# MIUI Update Blocker
+通过修改hosts的方式阻止com.android.updater联网
--- customize.sh
+++ customize.sh
@@ -1,6 +1,6 @@
 ui_print "### MIUI Update blocker"
 ui_print "Author: CxZMoE"
-ui_print "Stop com.miui.update from connecting to its server."
+ui_print "Stop com.android.updater from connecting to its server."
 
 # 设置hosts文件的权限为0644
 set_perm $MODPATH/system/etc/hosts 0 0 0644
